0ce9bec1e855cff649067731cc58fa1c8ef1b950
Nicklas Møller Jepsen <nicklas.m.jepsen@gmail.com>
2019-01-27 18:08:56 +0100

    added control controller

---
+++ BluOS.Web/Controllers/ControlController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BluOS.Web.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ControlController : BluOSController
+    {
+        public ControlController(IHttpClientFactory httpClientFactory) : base(httpClientFactory)
+        {
+        }
+
+        [HttpGet("Play")]
+        public async Task Play()
+        {
+            await WriteXMLResponse(await BluOSService.GetStringAsync("Play"));
+        }
+
+        [HttpGet("Pause")]
+        public async Task Pause()
+        {
+            await WriteXMLResponse(await BluOSService.GetStringAsync("Pause"));
+        }
+    }
+}
